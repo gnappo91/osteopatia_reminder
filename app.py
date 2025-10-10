dfb92fbf1a0e5d1f060f8151c08e025a7fd893b9
andrea-R <neri.a@fatergroup.com>
2025-10-10 14:47:13 +0200

    added session state with creds

--- app.py
+++ app.py
@@ -69,11 +69,15 @@ def create_appointment_summary(appointments):
 """
 
 st.title("Invia un messaggio di reminder a tutti i pazienti di domani")
-
+st.session_state["google_credentials"] = None
 if st.button("Trova contatti a cui inviare il messaggio"):
-    with st.spinner("Authenticating with Google..."):
-        creds = get_google_credentials()
+    if not st.session_state["google_gredentials"]:
+        with st.spinner("Authenticating with Google..."):
+            creds = get_google_credentials()
+            st.session_state["google_credentials"] = creds
 
+    else:
+        creds = st.session_state["google_credentials"]
     appointments = get_events(creds)
     if not appointments:
         st.write("Non ho trovato nessun paziente per domani")
